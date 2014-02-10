27a2fa1309989b5720454f197c26d6dbfa4bc82c
Thomas Henderson <tom@mathpunk.net>
2014-02-10 14:26:12 -0800

    Some more exploring of broken and swearful tag sets.

--- src/munger/en.clj
+++ src/munger/en.clj
@@ -55,10 +55,53 @@ fucked
 bullshit
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Operators, limits, etc;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def swears (m/find-maps "evernotes" {:tags {$regex ".*(shit|fuck).*" $options "i"}} ["id" "tags"]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:SWEARS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The classic "7 Dirty Words", or "the Carlin list":
+;; - shit
+;; - piss
+;; - cunt
+;; - fuck
+;; - cocksucker
+;; - motherfucker
+;; - tits
+
+;; We start with a modified Carlin list:
+;;
+;;         #"(shit|piss|cunt|fuck|cock|tits)"
+
+;; "Ass" is a problem, because it is a mild swear that can be said on broadcast television after 10pm. It turns out that the
+;; only "ass" tags that are not "class" or "assault" or whatever, as demonstrated with this:
+
+(let [sets (map #(:tags %) (m/find-maps "evernotes" {:tags {$regex "ass" $options "i"}} ["tags"]))
+      tags (flatten sets)]
+  (distinct (filter #(re-matches #".*ass.*" %) tags)))
+
+;; We find that the only real swears are "assholes" in a couple of variations. Therefore I include "assh" as something
+;; to watch for, because it will pick up both "assholes" and "asshats", in case such term arises. It will not pick up
+;; "assclowns" but it's not a perfect world. I've also added "dick" because I distinctly recall referring to tagging
+;; stories about actions of certain salami commanders with the word "dicks".
+;;
+;; So the final swears list shall be:
+
+(def dirty-words "(shit|piss|cunt|fuck|cock|tits|assh|dick)")
+
+;; Note that this is a string rather than a regex, because this is getting fed to monger's specialized operators.
+
+(map #(:tags %) (m/find-maps "evernotes" {:tags {$regex dirty-words $options "i"}} ["id" "tags"]))
 
 
+;; NOTE: SOmething is wrong, because there is allegedly a single document with like 40 tags?
+;; Which one is that?
+
+(def en (m/find-maps "evernotes" {:tags {$regex dirty-words $options "i"}} ["id" "tags"]))
+
+(filter #(> (count (:tags %)) 10) en)
+
+(def mess (m/find-one-as-map "evernotes" {:id 24932}))
 
 
-;; omg
+(count (:tags mess))
+
+
+;; Okay, that's one messed up note. So now I'm going to see if there are a lot of things with many many tags.
+;; Except, I don't know how. So I'll wait on that. I've added a note to our "special cases" list.
