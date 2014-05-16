bbf541f62548137a73a70848844a56f5a3c375e6
Thomas Henderson <tom@mathpunk.net>
2014-05-16 10:44:38 -0700

    Factored out the db connection (kinda)

--- src/munger/core.clj
+++ src/munger/core.clj
@@ -1,24 +1,9 @@
 (ns munger.core
+  (:use [munger.db :only [connect]])
   (:require [monger.collection :as m] [monger.core :as mc])
   (:use [monger.operators])
   (:import [org.bson.types ObjectId] [com.mongodb DB WriteConcern MongoOptions ServerAddress]))
 
-;; Connecting to the machine and database
-(defn connection [host db]
-  (let [^MongoOptions opts (mc/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
-        port 27017
-        ^ServerAddress sa  (mc/server-address host port)]
-    (do
-      (mc/connect! sa opts)
-      (mc/set-db! (mc/get-db db))
-      )
-    )
-  )
-
-(defn connect [ ]
-  (connection "feuille" "livre"))
-
-;; Presumably this is an arg to a function or whatever?
 (connect)
 
 
---
+++ src/munger/db.clj
@@ -0,0 +1,19 @@
+(ns munger.db
+  (:require [monger.collection :as m] [monger.core :as mc])
+  (:use [monger.operators])
+  (:import [org.bson.types ObjectId] [com.mongodb DB WriteConcern MongoOptions ServerAddress]))
+
+;; Connecting to the machine and database
+(defn connection [host db]
+  (let [^MongoOptions opts (mc/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
+        port 27017
+        ^ServerAddress sa  (mc/server-address host port)]
+    (do
+      (mc/connect! sa opts)
+      (mc/set-db! (mc/get-db db))
+      )
+    )
+  )
+
+(defn connect [ ]
+  (connection "feuille" "livre"))
